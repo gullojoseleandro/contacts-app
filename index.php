013f6709292c4264239b2e75842332d1a5859f6b
leandrosec19 <leandrosec19@gmail.com>
2023-11-19 23:21:24 +0000

    2

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
   $client = new Client([
   'url' => 'postgres://default:XxkvYUf7jVK6@ep-square-bar-06850403-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb',
 ]);
-
-  include "/home/user/contacts-app/static/contact.php"?>
-
+  $rows = $client->query('SELECT * FROM contacts');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
