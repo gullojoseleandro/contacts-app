9f61b3bedda2e118f6279d9162fb1df213826571
leandrosec19 <leandrosec19@gmail.com>
2023-11-19 21:13:57 +0000

    5to intento

--- static/database.php
+++ static/database.php
@@ -6,7 +6,8 @@ $user = "default";
 $password = "XxkvYUf7jVK6";
 
 try {
-  $conn = new PDO("pgsql:host=ep-square-bar-06850403.us-east-1.postgres.vercel-storage.com;dbname=verceldb;user=default;password=XxkvYUf7jVK6");
+  $conn = pg_connect("host=$host dbname=$database user=$user password=$password");
+
   echo "Conexión exitosa a la base de datos PostgreSQL";
 }catch(PDOException $e) {
   die("Error al conectar a la base de datos: " . $e-> getMessage());
